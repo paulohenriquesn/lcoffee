d18cf305ce598a5f95fe8f10384bbb49c35861dc
Paulo Henrique <gladgamer01@gmail.com>
2018-07-21 19:04:46 -0300

    var control system
    print "Hello World"
    x = 5
    print &x
    x += 5
    print &x
    x -= 5
    print &x
    x *= 2
    print &x
    x /= 2
    print &x

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ var readCodeText = require('read-text-file');
 var codeReaded = readCodeText.readSync('./script.lcoffee').split('\r\n');
 
 language = {
-    functions:['print'],
+    functions:[
+        'print',
+        'setvar',
+        'removevar',
+        'multiplyvar',
+        'dividevar'
+    ],
     vars:{
         int:{
         int_name:[],
@@ -30,6 +36,50 @@ lcoffee_execute = (func,value) =>
         }else {
             language.vars.int.int_name.push(value.split(',')[0]);
             language.vars.int.int_values.push(parseInt(value.split(',')[1]));
+        }
+        break;
+        case "addvar":
+        if(language.vars.int.int_name.includes(value.split(',')[0])){
+            pointer = 0;
+            for(x=0;x<language.vars.int.int_name.length;x++){
+                if(language.vars.int.int_name[x] == value.split(',')[0]){
+                    pointer = x;
+                }
+            }
+            language.vars.int.int_values[pointer] = language.vars.int.int_values[pointer] + parseInt(value.split(',')[1]);
+        }
+        break;
+        case "removevar":
+        if(language.vars.int.int_name.includes(value.split(',')[0])){
+            pointer = 0;
+            for(x=0;x<language.vars.int.int_name.length;x++){
+                if(language.vars.int.int_name[x] == value.split(',')[0]){
+                    pointer = x;
+                }
+            }
+            language.vars.int.int_values[pointer] = language.vars.int.int_values[pointer] - parseInt(value.split(',')[1]);
+        }
+        break;
+        case "multiplyvar":
+        if(language.vars.int.int_name.includes(value.split(',')[0])){
+            pointer = 0;
+            for(x=0;x<language.vars.int.int_name.length;x++){
+                if(language.vars.int.int_name[x] == value.split(',')[0]){
+                    pointer = x;
+                }
+            }
+            language.vars.int.int_values[pointer] = language.vars.int.int_values[pointer] * parseInt(value.split(',')[1]);
+        }
+        break;
+        case "dividevar":
+        if(language.vars.int.int_name.includes(value.split(',')[0])){
+            pointer = 0;
+            for(x=0;x<language.vars.int.int_name.length;x++){
+                if(language.vars.int.int_name[x] == value.split(',')[0]){
+                    pointer = x;
+                }
+            }
+            language.vars.int.int_values[pointer] = language.vars.int.int_values[pointer] / parseInt(value.split(',')[1]);
         }
         break;
     }
@@ -74,6 +124,26 @@ for (i = 0; i < codeReaded.length; i ++) {
 
     if(codeReaded[i].split(' ')[1] == '='){
         func = "setvar";
+        value = codeReaded[i].split(' ')[0] + "," + codeReaded[i].split(' ')[2];
+        lcoffee_execute(func,value); 
+    }
+    if(codeReaded[i].split(' ')[1] == '+='){
+        func = "addvar";
+        value = codeReaded[i].split(' ')[0] + "," + codeReaded[i].split(' ')[2];
+        lcoffee_execute(func,value); 
+    }
+    if(codeReaded[i].split(' ')[1] == '-='){
+        func = "removevar";
+        value = codeReaded[i].split(' ')[0] + "," + codeReaded[i].split(' ')[2];
+        lcoffee_execute(func,value); 
+    }
+    if(codeReaded[i].split(' ')[1] == '*='){
+        func = "multiplyvar";
+        value = codeReaded[i].split(' ')[0] + "," + codeReaded[i].split(' ')[2];
+        lcoffee_execute(func,value); 
+    }
+    if(codeReaded[i].split(' ')[1] == '/='){
+        func = "dividevar";
         value = codeReaded[i].split(' ')[0] + "," + codeReaded[i].split(' ')[2];
         lcoffee_execute(func,value); 
     }
